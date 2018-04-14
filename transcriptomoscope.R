d462d569f9e164343c60093a734fa5a20066dd27
Jonas Maaskola <jonas.maaskola@scilifelab.se>
2018-04-14 20:07:56 +0200

    Enable choice for plot order

--- transcriptomoscope.R
+++ transcriptomoscope.R
@@ -3,14 +3,20 @@ library("deldir")
 library("optparse")
 
 option_list = list(
+  make_option(c("-1", "--one"), type="logical", default=FALSE, action="store_true",
+              help="create one plot by interpreting three columns as red, green, and blue values. use this to plot dimensionality reduction results"),
+  make_option(c("-O", "--order"), type="logical", default=TRUE, action="store_false",
+              help="change plotting order: pages correspond to samples, and panels to columns; by default pages correspond to columns and panels to samples."),
   make_option(c("-c", "--columns"), type="numeric", default=NULL,
               help="number of columns in grid plots [default = ceiling(sqrt(n)), where n is the number of samples]",
               metavar="N"),
-  make_option(c("", "--convhull"), type="numeric", default=0.25,
+  make_option(c("-C", "--convhull"), type="numeric", default=0.25,
               help="distance to additional points to enlarge convex hull [default = 0.25]",
               metavar="N"),
   make_option(c("-b", "--blackbg"), type="logical", default=FALSE,
               help="create plots with a black background", action="store_true"),
+  make_option(c("", "--abs"), type="logical", default=FALSE,
+              help="plot absolute values when not using --one", action="store_true"),
   make_option(c("-t", "--transpose"), type="logical", default=FALSE,
               help="ensure that genes are rows and columns are spots",
               action="store_true"),
@@ -36,6 +42,8 @@ opt = parse_args(opt_parser, positional_arguments = c(1, Inf));
 
 #-------------------------------------------------------------------------------
 
+one.pic.mode = opt$options$one
+plot.order.col = opt$options$order
 ncols = opt$options$columns
 black.bg = opt$options$blackbg
 transpose = opt$options$transpose
@@ -46,6 +54,7 @@ bwinv = opt$options$invert
 sdims = opt$options$selectdims
 draw.border = opt$options$border
 convhull.distance = opt$options$convhull
+relative.frequency = !opt$options$abs & !one.pic.mode
 
 palettes <- list(
   green.to.blue = colorRamp(brewer.pal(9,"GnBu")),
@@ -71,8 +80,12 @@ paths = opt$args
 st.load.matrix <- function(path)
   as.matrix(read.table(file = path, sep="\t", header = T, row.names = 1))
 
+parse.coords <- function(n, delimiter=coord.delimiter)
+  apply(do.call(rbind, strsplit(n, split = "x")), 2, as.numeric)
+
 # Load files
 d <- list()
+coords <- list()
 for(path in paths) {
   d[[path]] = st.load.matrix(path)
   if (transpose) {
@@ -81,14 +94,20 @@ for(path in paths) {
   if (!is.null(sdims)) {
     d[[path]] <- d[[path]][, ind]
   }
+  coords[[path]] <- parse.coords(rownames(d[[path]]))
+  coords[[path]][,2] <- 36 - coords[[path]][,2]
+  if (relative.frequency)
+    d[[path]] = prop.table(d[[path]], 1)
 }
 
+ncolumns = ncol(d[[1]])
 # Set dimensions of plot
-if (ncol(d[[1]]) > 3) {
-  n = ncol(d[[1]])
-} else {
+if(one.pic.mode | plot.order.col) {
   n = length(d)
+} else {
+  n = ncolumns
 }
+
 if (!is.null(ncols)) {
   nc = ncols
   nr = ceiling(n/ncols)
@@ -101,44 +120,79 @@ mins = apply(sapply(d, function(x) apply(x, 2, min)), 1, min)
 maxs = apply(sapply(d, function(x) apply(x, 2, max)), 1, max)
 ranges = maxs - mins
 
-parse.coords <- function(n, delimiter=coord.delimiter)
-  apply(do.call(rbind, strsplit(n, split = "x")), 2, as.numeric)
-
-pdf(file = outpath, width = 6*nc, height = 6*nr)
-par(mfrow = c(nr, nc), mar = c(0,0,0,0), bg = ifelse(black.bg, "black", "white"))
-for(path in paths) {
-  print(path)
-  coords <- parse.coords(rownames(d[[path]]))
-  x <- coords[, 1]
-  y <- coords[, 2]
-  y <- 36 - y
-  vtess <- deldir(x, y)
-  tiles <- tile.list(vtess)
-  z = d[[path]]
-  z = t((t(z) - mins) / ranges)
-  a <- convhull.distance
+enlarged.convex.hull <- function(x, y, a) {
   X <- c(x, x + a, x, x - a)
   Y <- c(y + a, y, y - a, y)
   cpi <- chull(X, Y)
-  cp <- list(x = X[cpi], y = Y[cpi])
+  list(x = X[cpi], y = Y[cpi])
+}
 
-  # Plot each column in Grayscale if there are more than 3 columns
-  if (ncol(z) > 3) {
-    par(mfrow = c(nr, nc), mar = c(0, 0, 0, 0), bg = ifelse(black.bg, "black", "white"))
-    for (i in 1:ncol(z)) {
-      v <- z[, i]
-      plot(x, y, type = 'n', bty = 'none', axes = FALSE, xlim = c(0, 34), ylim = c(0, 36))
-      if (bwinv) {
-        v <- 1 - v
+vtess <- list()
+tiles <- list()
+z <- list()
+cp <- list()
+
+for(path in paths) {
+  print(path)
+  x <- coords[[path]][, 1]
+  y <- coords[[path]][, 2]
+  vtess <- deldir(x, y)
+  tiles[[path]] <- tile.list(vtess)
+  z[[path]] = d[[path]]
+  z[[path]] = t((t(z[[path]]) - mins) / ranges)
+
+  cp[[path]] <- enlarged.convex.hull(x, y, convhull.distance)
+}
+
+pl <- NULL
+if(one.pic.mode) {
+  pl <- function(fn) {
+    par(mfrow = c(nr, nc))
+    for(path in paths)
+      fn(path, NULL)
+  }
+} else {
+  if(plot.order.col) {
+    pl <- function(fn) {
+      for(col in 1:ncolumns) {
+        par(mfrow = c(nr, nc))
+        for(path in paths)
+          fn(path, col)
       }
-      cols <- rgb(palette(v), maxColorValue=255)
-      plot(tiles, fillcol = cols, showpoints = FALSE, border = ifelse(draw.border, TRUE, NA), clipp = cp, add = TRUE)
     }
   } else {
-    while (ncol(z) < 3)
-      z <- cbind(z, 0)
-    plot(x, y, type = 'n', bty = 'none', axes = FALSE, xlim = c(0, 34), ylim = c(0, 36))
-    plot(tiles, fillcol = rgb(z), showpoints = FALSE, border = ifelse(draw.border, TRUE, NA), clipp = cp, add = TRUE)
+    pl <- function(fn) {
+      for(path in paths) {
+        par(mfrow = c(nr, nc))
+        for(col in 1:ncolumns)
+          fn(path, col)
+      }
+    }
   }
 }
+
+pdf(file = outpath, width = 6*nc, height = 6*nr)
+par(mar = c(0, 0, 0, 0), bg = ifelse(black.bg, "black", "white"))
+
+make.plot = function(path, col) {
+  print(c(path, col))
+  x <- coords[[path]][, 1]
+  y <- coords[[path]][, 2]
+  if(one.pic.mode) {
+    while (ncol(z[[path]]) < 3)
+      z[[path]] <- cbind(z[[path]], 0)
+    plot(x, y, type = 'n', bty = 'none', axes = FALSE, xlim = c(0, 34), ylim = c(0, 36))
+    plot(tiles[[path]], fillcol = rgb(z[[path]][,1:3]), showpoints = FALSE, border = ifelse(draw.border, TRUE, NA), clipp = cp[[path]], add = TRUE)
+  } else {
+    v <- z[[path]][, col]
+    plot(x, y, type = 'n', bty = 'none', axes = FALSE, xlim = c(0, 34), ylim = c(0, 36))
+    if (bwinv) {
+      v <- 1 - v
+    }
+    cols <- rgb(palette(v), maxColorValue=255)
+    plot(tiles[[path]], fillcol = cols, showpoints = FALSE, border = ifelse(draw.border, TRUE, NA), clipp = cp[[path]], add = TRUE)
+  }
+}
+
+pl(make.plot)
 dev.off()
