ff4d8c3247b87ef6f06d6b2d4b6b688e205f42f1
Jonas Maaskola <jonas.maaskola@scilifelab.se>
2018-04-18 21:35:38 +0200

    Properly close the outlines

--- transcriptomoscope.R
+++ transcriptomoscope.R
@@ -249,8 +249,10 @@ make.plot = function(path, col) {
     }
     plot(tiles[[path]], fillcol = cols, showpoints = FALSE, border = ifelse(draw.border, TRUE, NA), clipp = cp[[path]], add = TRUE)
   }
-  if (opt$options$outline)
-    lines(rbind(cp[[path]],cp[[path]][1,]), col='black')
+  if (opt$options$outline) {
+    pts = cbind(cp[[path]]$x, cp[[path]]$y)
+    lines(rbind(pts, pts[1,]), col='black')
+  }
   if (opt$options$points)
     points(coords[[path]], col='black', cex=2)
 }
